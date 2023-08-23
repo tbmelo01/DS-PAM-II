ea349dc44ecf12dfe83e3d2c2b36d5f1a1bf6e48
tbmelo01 <batistamelo2012@hotmail.com>
2023-08-23 20:02:46 -0300

    Aulas de introdução ao javascript

---
+++ IntroducaoAoJs/Aula02/Aula02.js
@@ -0,0 +1,20 @@
+const tb = {
+    nome: "tiago",
+    signo: "Gêmeos",
+    idade: 31,
+    doido: false,
+    animes: ["Naruto", "bleach"] 
+}
+
+//formato Json - JavaScript Notation object
+
+console.log(tb.nome, tb.signo);
+console.log(tb);
+console.log(typeof tb);
+console.log(tb.animes);
+
+
+let pessoa = ["tiago", 31, "estudante"];
+console.log(pessoa[0], pessoa[2]);
+console.log(pessoa);
+console.log(typeof pessoa);
