1336964c20ca62f6e50f01c84778c57602f5d68e
tbmelo01 <batistamelo2012@hotmail.com>
2023-09-11 22:41:35 -0300

    primeiro projeto funcional em react native

--- IntroducaoAoJs/Aula01 react/meu-primeiro-app/App.js
+++ IntroducaoAoJs/Aula01 react/meu-primeiro-app/App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as react from 'react';
+import {text, view, stylesheet } from 'react-native';
+import Contador from './components/contador'
 
-export default function App() {
+export default function app() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <view>
+      <contador />
+    </view>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
