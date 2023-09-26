cc4c40f410f9e5c20805947655e232351454a9bf
tbmelo01 <batistamelo2012@hotmail.com>
2023-09-25 22:21:33 -0300

    aula sobre o contador

--- IntroducaoAoJs/Aula01 react/meu-primeiro-app/App.js
+++ IntroducaoAoJs/Aula01 react/meu-primeiro-app/App.js
@@ -1,11 +1,11 @@
 import * as react from 'react';
-import {text, view, stylesheet } from 'react-native';
-import Contador from './components/contador'
+import {Text, View, StyleSheet } from 'react-native';
+import Contador from './components/Contador'
 
 export default function app() {
   return (
     <view>
-      <contador />
+      <Contador />
     </view>
   );
 }
